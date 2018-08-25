7aee8fd65079b9b53f0fc1e71b573e33621cb4f5
Alicia Vissuet <aliciavissuet@Alicias-MacBook-Pro.local>
2018-08-25 12:35:46 -0500

    added profile

--- Config/keys.js
+++ Config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://alicia:alicia1@ds133262.mlab.com:33262/jsfullstackpractice'
+    mongoURI: 'mongodb://alicia:alicia1@ds133262.mlab.com:33262/jsfullstackpractice',
+    secretOrKey: 'secret'
 }
