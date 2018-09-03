1801a971e8e8ba84f3435912c96cedceed7e74d4
Alicia Vissuet <aliciavissuet@Alicias-MacBook-Pro.local>
2018-09-03 17:32:41 -0500

    Prepared for deploy

--- Config/keys.js
+++ Config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-    mongoURI: 'mongodb://aliciavissuet:alicia1@ds133262.mlab.com:33262/jsfullstackpractice',
-    secretOrKey: 'secret'
+if(process.env.NODE_ENV === 'production'){
+    module.exports = require('./keys_prod');
+} else {
+    module.exports = require('./keys_dev');
 }
